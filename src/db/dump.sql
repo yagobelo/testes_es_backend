b545db74f67dcffcbbfee50b9c12e9084655f384
yagobelo <yago.belo00@gmail.com>
2024-05-12 17:54:24 -0300

    chore: initial commit / initial config

---
+++ src/db/dump.sql
@@ -0,0 +1,34 @@
+create table
+  hospedes (
+    id serial primary key,
+    nome text not null,
+    data_nascimento text not null,
+    telefone text not null,
+    email text,
+    cpf text not null,
+    pais text not null,
+    estado text not null,
+    cidade text not null,
+    logradouro text not null,
+    numero_endereco text,
+    bairro text not null,
+    complemento_endereco text
+  );
+
+create table
+  reservas (
+    id serial primary key,
+    numero_reserva text not null,
+    data_checkin text not null,
+    data_checkout text not null,
+    status text not null
+  );
+
+create table
+  reserva_hospedes (
+    id serial primary key,
+    hospedes_id int not null,
+    reservas_id int not null,
+    foreign key (hospedes_id) references hospedes (id),
+    foreign key (reservas_id) references reservas (id)
+  );
---
+++ src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// const routes = require("./routes/routes");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+// server.use(routes);
+
+server.listen(process.env.PORT || 3000, () =>
+  console.log(`Server running in port: ${process.env.PORT}`)
+);

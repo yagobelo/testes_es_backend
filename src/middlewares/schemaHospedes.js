a78fe9edf9424d49994dc0df69c1d8198bbdc1d7
yagobelo <yago.belo00@gmail.com>
2024-05-13 12:21:41 -0300

    feat: adicionado validação de dados ao cadastrar novo hospede; chore: adicionado a lib joi

---
+++ src/middlewares/schemaHospedes.js
@@ -0,0 +1,64 @@
+const Joi = require("joi");
+
+const nome = Joi.string().required().messages({
+  "any.required": "O campo nome é obrigatório.",
+  "string.empty": "O campo nome é obrigatório.",
+});
+const data_nascimento = Joi.string().required().messages({
+  "any.required": "O campo data de nascimento é obrigatório.",
+  "string.empty": "O campo data de nascimento é obrigatório.",
+});
+const telefone = Joi.string().required().messages({
+  "any.required": "O campo telefone é obrigatório.",
+  "string.empty": "O campo telefone é obrigatório.",
+});
+const email = Joi.string().required().messages({
+  "any.required": "O campo email é obrigatório.",
+  "string.empty": "O campo email é obrigatório.",
+});
+const rg = Joi.string().required().messages({
+  "any.required": "O campo cpf é obrigatório.",
+  "string.empty": "O campo cpf é obrigatório.",
+});
+const pais = Joi.string().required().messages({
+  "any.required": "O campo país é obrigatório.",
+  "string.empty": "O campo país é obrigatório.",
+});
+const estado = Joi.string().required().messages({
+  "any.required": "O campo estado é obrigatório.",
+  "string.empty": "O campo estado é obrigatório.",
+});
+const cidade = Joi.string().required().messages({
+  "any.required": "O campo cidade é obrigatório.",
+  "string.empty": "O campo cidade é obrigatório.",
+});
+const logradouro = Joi.string().required().messages({
+  "any.required": "O campo logradouro é obrigatório.",
+  "string.empty": "O campo logradouro é obrigatório.",
+});
+const numero_endereco = Joi.string().required().messages({
+  "any.required": "O campo número é obrigatório.",
+  "string.empty": "O campo número é obrigatório.",
+});
+const bairro = Joi.string().required().messages({
+  "any.required": "O campo bairro é obrigatório.",
+  "string.empty": "O campo bairro é obrigatório.",
+});
+const complemento_endereco = Joi.any();
+
+const schemaHospedes = Joi.object({
+  nome,
+  data_nascimento,
+  telefone,
+  email,
+  rg,
+  pais,
+  estado,
+  cidade,
+  logradouro,
+  numero_endereco,
+  bairro,
+  complemento_endereco,
+});
+
+module.exports = schemaHospedes;
--- src/routes/hospedesRoutes.js
+++ src/routes/hospedesRoutes.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 
 const hospedesControllers = require("../controllers/hospedesControllers");
+const valid = require("../middlewares/valid");
+const schemaHospedes = require("../middlewares/schemaHospedes");
 
-router.post("/cadastro", hospedesControllers.cadastrarHospedes);
+router.post(
+  "/cadastro",
+  valid(schemaHospedes),
+  hospedesControllers.cadastrarHospedes
+);
 router.get("/listar", hospedesControllers.listarHospedes);
 router.put("/editar/:id", hospedesControllers.editarHospedes);
 
